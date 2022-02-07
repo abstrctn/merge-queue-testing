37a2133cf6d2548c88f15ea812df26f74fb8fe57
Joshua Hunsche Jones <joshua@hunschejones.com>
2022-02-07 15:46:53 -0600

    Update sleep.rb

--- test/sleep.rb
+++ test/sleep.rb
@@ -1,10 +1,15 @@
 puts "REF is equal to '#{ENV["REF"]}'" if ENV["REF"]
 
-if ENV["PR_NAME"] && ENV["PR_NAME"].downcase.include?("fail")
+if ENV["PR_NAME"] && ENV["PR_NAME"].downcase.include?("fail in the queue")
+  # REF is equal to 'refs/heads/gh-readonly-queue/main/pr-4-14e30f6938f96698309aec5bb5a293b0f5343e8a'
+  if ENV["PR_NAME"].downcase.include?("refs/heads/gh-readonly-queue")
+    raise "Manually failing in the merge queue due to PR name" 
+  end
+elsif ENV["PR_NAME"] && ENV["PR_NAME"].downcase.include?("fail")
   raise "Manually failing PR due to PR name"
-# elsif ENV["PR_NUMBER"] && ENV["PR_NUMBER"].to_i.odd?
-#   raise "Manually failing PR due to PR number"
-elsif ENV["TEST_DELAY_TIME_SECONDS"].to_s.empty?
+end
+
+if ENV["TEST_DELAY_TIME_SECONDS"].to_s.empty?
   puts "Using default sleep time"
   sleep 10
 else
