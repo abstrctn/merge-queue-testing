7a2f937f13063fe5014c3c811a8b3733e074491e
Joshua Hunsche Jones <joshua@hunschejones.com>
2022-02-07 16:06:36 -0600

    Update sleep.rb

--- test/sleep.rb
+++ test/sleep.rb
@@ -3,12 +3,14 @@ puts "REF is equal to '#{ENV["REF"]}'" if ENV["REF"]
 puts "PR_TITLE is equal to '#{ENV["PR_TITLE"]}" if ENV["PR_TITLE"]
 
 # Decide if setup values mean test should fail
-if ENV["PR_TITLE"] && ENV["REF"] && ENV["PR_TITLE"].downcase.include?("fail in the queue")
-  # REF is equal to 'refs/heads/gh-readonly-queue/main/pr-4-14e30f6938f96698309aec5bb5a293b0f5343e8a'
+#
+# PR_TITLE is empty for PR's in the merge queue 🤷🏼
+if ENV["REF"] && (ENV["PR_TITLE"].downcase.include?("fail in the queue") || ENV["PR_TITLE"].empty?)
+  # REF looks like 'refs/heads/gh-readonly-queue/main/pr-4-14e30f6938f96698309aec5bb5a293b0f5343e8a'
   if ENV["REF"].downcase.include?("refs/heads/gh-readonly-queue")
     raise "Manually failing in the merge queue due to PR title" 
   end
-elsif ENV["PR_TITLE"] && ENV["PR_TITLE"].downcase.include?("fail")
+elsif ENV["PR_TITLE"] && ENV["PR_TITLE"].downcase.include?("fail the pr")
   raise "Manually failing PR due to PR title"
 end
 
