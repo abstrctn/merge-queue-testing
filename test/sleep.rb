0ec632b5a3efdf4ec14f6b5105fd3388437149b8
Joshua Hunsche Jones <joshua@hunschejones.com>
2022-02-07 16:41:24 -0600

    Update sleep.rb

--- test/sleep.rb
+++ test/sleep.rb
@@ -1,8 +1,14 @@
 # Debugging setup values
-puts "REF is set to '#{ENV["REF"]}'" if ENV["REF"]
-puts "PR_TITLE is set to '#{ENV["PR_TITLE"]}'" if ENV["PR_TITLE"]
-puts "HEAD_REF is set to '#{ENV["HEAD_REF"]}'" if ENV["HEAD_REF"]
-puts "PR_NUMBER is set to '#{ENV["PR_NUMBER"]}'" if ENV["PR_NUMBER"]
+puts "REF is set to '#{ENV["REF"]}'"
+puts "PR_TITLE is set to '#{ENV["PR_TITLE"]}'"
+puts "HEAD_REF is set to '#{ENV["HEAD_REF"]}'"
+puts "PR_NUMBER is set to '#{ENV["PR_NUMBER"]}'"
+
+PR_FROM_MQ_REF_REGEX = /refs\/heads\/gh-readonly-queue\/main\/pr-(\d+)-/
+if ENV["PR_NUMBER"].empty? && ENV["REF"].match?(PR_FROM_MQ_REF_REGEX)
+  ENV["PR_NUMBER"] = ENV["REF"].match(PR_FROM_MQ_REF_REGEX)[1]
+  puts "PR_NUMBER was updated to '#{ENV["PR_NUMBER"]}'"
+end
 
 manual_failure = nil
 
@@ -10,7 +16,6 @@ manual_failure = nil
 #
 # REF looks like 'refs/heads/gh-readonly-queue/main/pr-4-14e30f6938f96698309aec5bb5a293b0f5343e8a'
 if ENV["REF"] && ENV["REF"].downcase.include?("refs/heads/gh-readonly-queue")
-  
   if ENV["PR_NUMBER"] && ENV["PR_NUMBER"].to_i.odd?
     manual_failure = "Manually failing in the merge queue due to PR title and PR number" 
   end
