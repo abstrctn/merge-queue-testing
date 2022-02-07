4578d5fd72185b230f060965a79eaccf499d6a72
Joshua Hunsche Jones <joshua@hunschejones.com>
2022-02-07 16:11:49 -0600

    Update sleep.rb

--- test/sleep.rb
+++ test/sleep.rb
@@ -1,6 +1,8 @@
 # Debugging setup values
-puts "REF is equal to '#{ENV["REF"]}'" if ENV["REF"]
-puts "PR_TITLE is equal to '#{ENV["PR_TITLE"]}'" if ENV["PR_TITLE"]
+puts "REF is set to '#{ENV["REF"]}'" if ENV["REF"]
+puts "PR_TITLE is set to '#{ENV["PR_TITLE"]}'" if ENV["PR_TITLE"]
+
+manual_failure = nil
 
 # Decide if setup values mean test should fail
 #
@@ -8,17 +10,19 @@ puts "PR_TITLE is equal to '#{ENV["PR_TITLE"]}'" if ENV["PR_TITLE"]
 if ENV["REF"] && (ENV["PR_TITLE"].downcase.include?("fail in the queue") || ENV["PR_TITLE"].empty?)
   # REF looks like 'refs/heads/gh-readonly-queue/main/pr-4-14e30f6938f96698309aec5bb5a293b0f5343e8a'
   if ENV["REF"].downcase.include?("refs/heads/gh-readonly-queue")
-    raise "Manually failing in the merge queue due to PR title" 
+    manual_failure = "Manually failing in the merge queue due to PR title" 
   end
 elsif ENV["PR_TITLE"] && ENV["PR_TITLE"].downcase.include?("fail the pr")
-  raise "Manually failing PR due to PR title"
+  manual_failure = "Manually failing PR due to PR title"
 end
 
-# Set the sleep time
+# Set the sleep time and raise manual_failure if set
 if ENV["TEST_DELAY_TIME_SECONDS"].to_s.empty?
   puts "Using default sleep time"
   sleep 10
+  raise manual_failure if manual_failure
 else
   puts "Sleep time set to #{ENV["TEST_DELAY_TIME_SECONDS"].to_i} via repository secret"
   sleep ENV["TEST_DELAY_TIME_SECONDS"].to_i
+  raise manual_failure if manual_failure
 end
