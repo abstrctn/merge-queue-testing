d57e6af973ba40ea4061fba2b31fc2093418f16d
Joshua Hunsche Jones <joshua@hunschejones.com>
2022-02-16 10:24:38 -0600

    Update fail.rb

--- test/fail.rb
+++ test/fail.rb
@@ -1,9 +1,9 @@
 if ENV["TEST_DELAY_TIME_SECONDS"].to_s.empty?
   puts "Using default sleep time"
   sleep 10
-  raise "This test always fails"
+  raise "This test is configured to fail" if ENV["NOT_REQUIRED_TEST_FAILING"].downcase == "true"
 else
   puts "Sleep time set to #{ENV["TEST_DELAY_TIME_SECONDS"].to_i} via repository secret"
   sleep ENV["TEST_DELAY_TIME_SECONDS"].to_i
-  raise "This test always fails"
+  raise "This test always fails" if ENV["NOT_REQUIRED_TEST_FAILING"].downcase == "true"
 end
