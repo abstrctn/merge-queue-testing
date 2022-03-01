9329f48dd3d0b4ab18c25d28b50cd6f8ec08e716
Joshua Hunsche Jones <jhunschejones@github.com>
2022-03-01 10:00:52 -0600

    Reset main, add second test script

--- test/validation_test.rb
+++ test/validation_test.rb
@@ -7,9 +7,11 @@ describe Adder do
     sleep ENV["TEST_DELAY_TIME_SECONDS"].to_i if ENV["TEST_DELAY_TIME_SECONDS"]
   end
 
-  describe "#add" do
-    it "adds two values" do
-      assert_equal 3, @adder.add(1, 2)
+  describe "#subtract" do
+    it "raises for missing method" do
+      error = assert_raises(NoMethodError) do
+        @adder.subtract(1, 2)
+      end
     end
   end
 end
